4e7b0486161d6339e6678d34b20dbd4af8d9aec3
Zacke88 <zakrisson88@gmail.com>
2016-09-13 12:50:46 +0200

    Lighter code

--- app/src/main/java/com/example/zacke/greed/MainActivity.java
+++ app/src/main/java/com/example/zacke/greed/MainActivity.java
@@ -5,7 +5,6 @@ import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
